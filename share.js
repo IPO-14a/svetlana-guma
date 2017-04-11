5b1140271189e701e091ac44288229ebfff100f5
Svetlana Guma <dunno183@gmail.com>
2017-04-11 20:25:30 +0300

    share selected text, links and pages; minor popup changes

--- share.js
+++ share.js
@@ -1,9 +1,27 @@
-var context = "image";
-var titlex = "Share on Twitter..."
+var contextsList = ["selected text","link", "image", "page"];
+
 //добавление пунктов контекстного меню в Chrome
-chrome.contextMenus.create( {title: titlex,contexts:[context],onclick: clickHandler,id: context});
+for(i = 0; i  < contextsList.length; i++) {
+	var context = contextsList[i];
+	var titlex = "Share " + context + " on Twitter";
+	chrome.contextMenus.create( {title: titlex,contexts:[context],onclick: clickHandler,id: context});
+}
 
 function clickHandler(data, tab) {
-	//поделиться изображением
-	chrome.windows.create({url : "https://twitter.com/intent/tweet?url="+encodeURIComponent(data.srcUrl), type : "panel"});
+	//поделиться элементами
+	switch(data.menuItemId) {
+    case 'selected text' :
+    chrome.windows.create({url : "https://twitter.com/intent/tweet?text="+encodeURIComponent(data.selectionText), type : "panel"} );
+    break;
+    case 'link' :
+    chrome.windows.create({url : "https://twitter.com/intent/tweet?url="+encodeURIComponent(data.linkUrl), type : "panel"} );
+    break;
+    case 'image' :
+    chrome.windows.create({url : "https://twitter.com/intent/tweet?url="+encodeURIComponent(data.srcUrl), type : "panel"});
+    break;
+    case 'page' :
+    chrome.windows.create({url : "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+(data.pageUrl), type : "panel"});
+    break;
+
+  }
 }
