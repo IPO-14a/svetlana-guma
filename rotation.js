b9435c360a8850b6a17444f59fba17481ac41899
Svetlana Guma <dunno183@gmail.com>
2017-04-25 20:06:32 +0300

    twitter_logo rotation

---
+++ rotation.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){jQuery("#rotateImg").rotate({ 
+   bind: 
+     { 
+        mouseover : function() { 
+            jQuery(this).rotate({animateTo:360})
+        },
+        mouseout : function() { 
+            jQuery(this).rotate({animateTo:0})
+        }
+     } 
+})
+});
