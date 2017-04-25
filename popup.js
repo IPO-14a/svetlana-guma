a25b60fe49b2de7cf4ee6fd39345df71de0db19e
Svetlana Guma <dunno183@gmail.com>
2017-04-25 20:24:36 +0300

    again

--- popup.js
+++ popup.js
@@ -5,26 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
         (function () {
             var ln = links[i];
             var location = ln.href;
-            ln.onclick = function () {
+            ln.onclick = function () {$('#tweetText').val();
                 //создание новой вкладки при нажании на ссылку
                 chrome.tabs.create({active: true, url: location});
             };
         })();
     }
 });
-
-var datTweet = " ";
- 
- $(document).ready(function () {
-     $('#btnTweet').click(function (e) {
-         datTweet=window.prompt("Type Your Tweet Below...Press OK To Review Your Tweet Before Sending It (opens twitter in new window)")
-         //var textToTweet = "Hi I am tweeting from here";
-         if (datTweet.length > 140) {
-             alert('Tweet should be less than 140 Chars');
-         }
-         else {
-             var twtLink = 'http://twitter.com/home?status=' +encodeURIComponent(datTweet);
-             window.open(twtLink,'_blank');
-         }
-     });
- });
