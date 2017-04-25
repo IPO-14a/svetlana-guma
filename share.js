d8f8f682f3b24dbe1da15c62e95a02dfe87d1d4a
Svetlana Guma <dunno183@gmail.com>
2017-04-25 20:05:17 +0300

    logo added, popup updated

---
+++ share.js
@@ -0,0 +1,27 @@
+var contextsList = ["selected text","link", "image", "page"];
+
+//добавление пунктов контекстного меню в Chrome
+for (i = 0; i  < contextsList.length; i++) {
+    var context = contextsList[i];
+    var titlex = "Share " + context + " on Twitter";
+    chrome.contextMenus.create( {title: titlex, contexts:[context], onclick: clickHandler, id: context});
+}
+
+//обработка пунктов контекстного меню
+function clickHandler(data, tab) {
+    //поделиться элементами
+    switch(data.menuItemId) {
+    case 'image' :
+    chrome.windows.create({url : "https://twitter.com/intent/tweet?url=" + encodeURIComponent(data.srcUrl), type : "panel"});
+    break;
+    case 'selected text' :
+    chrome.windows.create({ url : "https://twitter.com/intent/tweet?text=" + encodeURIComponent(data.selectionText), type : "panel"} );
+    break;
+    case 'link' :
+    chrome.windows.create({url : "https://twitter.com/intent/tweet?url=" + encodeURIComponent(data.linkUrl), type : "panel"} );
+    break;
+    case 'page' :
+    chrome.windows.create({url : "https://twitter.com/intent/tweet?text=" + encodeURIComponent(tab.title) + "&url=" + (data.pageUrl), type : "panel"});
+    break;
+  }
+}
